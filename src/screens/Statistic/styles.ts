fde475dc74a18415bd8dced8e78e1c77508353d8
Junior Sousa <jrsousadev@gmail.com>
2023-01-30 18:09:29 -0300

    creating structure and finished firsts screens

---
+++ src/screens/Statistic/styles.ts
@@ -0,0 +1,35 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+  height: 100%;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+
+  margin-top: 20px;
+
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.WHITE};
+  `}
+
+  align-items: center;
+
+  padding-top: 33px;
+
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.BLACK};
+  `}
+
+  padding-bottom: 18px;
+`
+
+export const ContainerTwoCard = styled.View`
+  flex-direction: row;
+  width: 100%;
+`
---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    GREEN: "#639339",
+    GREEN_200: "#E5F0DB",
+    GREEN_400: "#CBE4B4",
+
+    BLACK: "#333638",
+
+    BACKGROUND: "#FAFAFA",
+
+    GRAY_200: "#EFF0F0",
+    GRAY_500: "#DDDEDF",
+
+    RED: "#BF3B44",
+    RED_200: "#F4E6E7",
+    RED_400: "#F3BABD",
+  },
+  FONT_FAMILY: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XLL: 30,
+    XXL: 34,
+  },
+};
