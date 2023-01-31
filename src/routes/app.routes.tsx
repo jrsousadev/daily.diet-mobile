1c291e0aadd7cd918add7d5373b23762c99f99c0
Junior S <jrsousadev@gmail.com>
2023-01-31 00:18:26 -0300

    initing creation screen new feed

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "../screens/Home";
 import { Static } from "@screens/Statistic";
+import { CreateFeed } from "@screens/CreateFeed";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export function AppRoutes() {
     >
       <Screen name="home" component={HomeScreen} />
       <Screen name="static" component={Static} />
+      <Screen name="createFeed" component={CreateFeed} />
     </Navigator>
   );
 }
