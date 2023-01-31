aca8e945e12929f8c9f08106bdf8534dc990e86b
Junior Sousa <jrsousadev@gmail.com>
2023-01-31 12:54:22 -0300

    adding storage and create new functions

---
+++ src/storage/feed/getAllFeeds.ts
@@ -0,0 +1,16 @@
+import { FeedDTO } from "@domain/Feed";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { FEED_COLLECTION } from "@storage/storageConfig";
+
+export async function getAllFeeds() {
+  try {
+    const storage = await AsyncStorage.getItem(FEED_COLLECTION);
+
+    const feeds: FeedDTO[] = storage ? JSON.parse(storage) : [];
+
+    return feeds.reverse();
+  } catch (err) {
+    throw err;
+  }
+}
