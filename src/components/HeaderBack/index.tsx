42c019db8524e5c566ab2d112b1c5db62184a295
Junior S <jrsousadev@gmail.com>
2023-01-31 00:18:10 -0300

    initing creation screen new feed

---
+++ src/components/HeaderBack/index.tsx
@@ -0,0 +1,25 @@
+import { useNavigation } from "@react-navigation/native";
+import * as S from "./styles";
+
+type Props = {
+    title: string;
+}
+
+export function HeaderBack({ title }: Props) {
+    const navigate = useNavigation();
+
+    const handleBack = () => {
+        navigate.goBack();
+    }
+
+    return (
+        <S.Container>
+            <S.ButtonBack onPress={handleBack}>
+                <S.IconBack />
+            </S.ButtonBack>
+            <S.Title>
+                {title}
+            </S.Title>
+        </S.Container>
+    )
+}
